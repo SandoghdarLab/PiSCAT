b96132ec25dbd7b45d84fd786af9fc52622bd22f
Houman Mirzaalian Dastjerdi <houman.mirzaalian-dastjerdi@mpl.mpg.de>
2021-04-26 16:59:16 +0200

    Adding logo

--- docs/conf.py
+++ docs/conf.py
@@ -80,10 +80,17 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "./Fig/PiSCAT_logo.png"
 
 html_css_files = [
     'widestyle.css',
 ]
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'titles_only': False,
+}
 
 nbsphinx_allow_errors = True
 
--- piscat/_version_save.py
+++ piscat/_version_save.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 # This file was created automatically
-longversion = '0.1.0-2-g6131ee1'
+longversion = '0.1.0-14-gc22fbf4'
