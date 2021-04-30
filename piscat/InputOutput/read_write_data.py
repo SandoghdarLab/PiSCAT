b5c850de2f334116a1b439ce8eca6670c8dd1de7
Houman Mirzaalian Dastjerdi <houman.mirzaalian-dastjerdi@mpl.mpg.de>
2021-04-30 15:32:07 +0200

    Fix Demo Tu3

--- piscat/InputOutput/read_write_data.py
+++ piscat/InputOutput/read_write_data.py
@@ -255,7 +255,7 @@ class download_tutorial_data():
                 print("Directory ", name_mkdir_2, " already exists!")
             else:
                 dr_ = os.path.join(save_path, name_mkdir_1)
-                self.run_download(url='https://owncloud.gwdg.de/index.php/s/Cq5vU8qIAFIWwEh/download', save_path=dr_)
+                self.run_download(url='https://owncloud.gwdg.de/index.php/s/KkGs00yZ7Xyx8pX/download', save_path=dr_)
 
             dr_mk = os.path.join(save_path, name_mkdir_1, name_mkdir_3)
 
