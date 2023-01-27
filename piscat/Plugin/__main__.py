3e51318fb0a964156b04fd6a3e58d2232b2f0452
Houman Mirzaalian Dastjerdi <houman.mirzaalian-dastjerdi@mpl.mpg.de>
2023-01-27 17:00:22 +0100

    edit the main file to clone file in a same directory

--- piscat/Plugin/__main__.py
+++ piscat/Plugin/__main__.py
@@ -62,9 +62,7 @@ class Plugin(PrintColors):
             Specify and plugin data changed or not
         """
         target = os.path.dirname(os.path.realpath(__file__))
-        print(target)
         target = os.path.join(target, plu_name)
-        print(target)
         if exists(target):
             print('Warning! Target directory %s already exists.' % str(target))
             answer = self.confirm(f'{self.BOLD}\n Target directory %s already exists. '
